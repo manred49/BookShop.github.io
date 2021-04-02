bb89cabc0522847c7468cdc43b03001f9ec4784f
lương lê <68262875+manred49@users.noreply.github.com>
2021-04-02 22:29:11 +0700

    update

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// // const btnOpenSign = document.getElementById('nav__itemssign');
+// // const openModal = document.getElementById('modal');
+
+// // btnOpenSign.addEventListener('click', function(){
+// //     openModal.classList.add('show')
+// // });
+
+// document.getElementById('nav__itemssign').addEventListener('click',function(){
+//     document.getElementById('.modal').style.display= 'flex' 
+// })
+console.log('hello')
+var atriHeading=document.getElementsByTagName('header')
+console.log(atriHeading.);
